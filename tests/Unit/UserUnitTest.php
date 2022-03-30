dc197c445b5f71306f8016c62613daed39fd1d3c
wuppsn <wuppsn@gmail.com>
2022-03-31 01:33:41 +0700

    test

--- tests/Unit/UserUnitTest.php
+++ tests/Unit/UserUnitTest.php
@@ -94,10 +94,39 @@ class UserUnitTest extends TestCase
     // test schema: can input another language (username)?
     // test schema: can input English language (username)?
     // test schema: can input only integer (username)?
+    public function test_user_input_name_only_integer(){
+        $user = new User([
+            'id' => 1,
+            'name' => '12345',
+
+        ]);
+
+        $this->assertEquals('12345', $user->name);
+    }
+
     // test schema: can input space (username)?
+    // public function test_user_input_space(){
+    //     $user = new User([
+    //         'id' => 1,
+    //         'name' => 'Do me',
+    //     ]);
+    //     $this->assertEquals('Do me', $user->name);
+    // }
+
     // test schema: can input another language (password)?
     // test schema: can input English language (password)?
+
     // test schema: can input only integer (password)?
+    // public function test_user_input_password_only_integer(){
+    //     $user = new User([
+    //         'id' => 1,
+    //         'password' => "12345"
+
+    //     ]);
+
+    //     $this->assertEquals('12345', $user->password);
+    // }
+
     // test schema: can input space (password)?
     // test schema: can input another language (email)?
     // test schema: can input English language (email)?
