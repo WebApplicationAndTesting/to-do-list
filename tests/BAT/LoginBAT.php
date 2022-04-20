62401ff816f3c26d0f64438e48736088a4cce199
Domeไงจะใครล่ะ <60128308+dome44-tp@users.noreply.github.com>
2022-04-20 12:38:23 +0700

    [CREATE] func. RegisterBAT & LoginBAT

--- tests/BAT/LoginBAT.php
+++ tests/BAT/LoginBAT.php
@@ -1,13 +1,55 @@
 <?php
     
 class LoginBAT {
-    // password NULL
-    // email NULL
-    // password input another lang
-    // email input another lang
-    // password input symbol
-    // email input symbol
-    // invalid email
-    // invalid password
+
+    // Do not enter any text in the email field.
+    public function test_do_not_input_text_in_email_field() {
+
+    }
+
+    // Do not enter any text in the password field.
+    public function test_do_not_input_text_in_password_field() {
+
+    }
+
+    // Enter another language in the email field.
+    public function test_input_another_language_in_email_field() {
+
+    }
+
+    // Enter another language in the password field.
+    public function test_input_another_language_in_password_field() {
+        
+    }
+
+    // Enter symbol in the email field.
+    public function test_input_symbol_in_email_field() {
+        
+    }
+
+    // Enter symbol in the password field.
+    public function test_input_symbol_in_password_field() {
+        
+    }
+
+    // Input invalid email
+    public function test_input_invalid_email() {
+        
+    }
+
+    // Input invalid password
+    public function test_input_invalid_password() {
+        
+    }
+
+    // Input another word (not .com)
+    public function test_user_input_another_word_end_email(){
+        
+    }
+
+    // Input another word (not @gmail ex. @beam.com)
+    public function test_user_input_another_word_if_not_gmail(){
+        
+    }    
 
 }
--- tests/BAT/RegisterBAT.php
+++ tests/BAT/RegisterBAT.php
@@ -1,17 +1,75 @@
 <?php
     
 class RegisterBAT {
-    // username NULL
-    // password NULL
-    // confirm password NULL
-    // email NULL
-    // username input another lang
-    // password input another lang
-    // confirm password input another lang
-    // email input another lang
-    // username input symbol
-    // password input symbol
-    // confirm password input symbol
-    // email input symbol
+
+    // Do not enter any text in the username field.
+    public function test_do_not_input_text_in_username_field() {
+
+    }
+
+    // Do not enter any text in the email field.
+    public function test_do_not_input_text_in_email_field() {
+
+    }
+
+    // Do not enter any text in the password field.
+    public function test_do_not_input_text_in_password_field() {
+
+    }
+
+    // Do not enter any text in the confirm password field.
+    public function test_do_not_input_text_in_confirm_password_field() {
+
+    }
+
+    // Enter another language in the username field.
+    public function test_input_another_language_in_username_field() {
+
+    }
+
+    // Enter another language in the email field.
+     public function test_input_another_language_in_email_field() {
+
+    }
+
+    // Enter another language in the password field.
+    public function test_input_another_language_in_password_field() {
+        
+    }
+
+    // Enter another language in the pconfirm assword field.
+    public function test_input_another_language_in_confirm_password_field() {
+        
+    }
+
+    // Enter symbol in the username field.
+    public function test_input_symbol_in_username_field() {
+            
+    }
+
+    // Enter symbol in the email field.
+    public function test_input_symbol_in_email_field() {
+        
+    }
+
+    // Enter symbol in the password field.
+    public function test_input_symbol_in_password_field() {
+        
+    }
+
+    // Enter symbol in the confirm password field.
+    public function test_input_symbol_in_confirm_password_field() {
+        
+    }
+
+    // Input another word (not .com)
+    public function test_user_input_another_word_end_email(){
+        
+    }
+
+    // Input another word (not @gmail ex. @beam.com)
+    public function test_user_input_another_word_if_not_gmail(){
+        
+    }    
 
 }
