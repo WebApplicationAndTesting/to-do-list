d208163e10d5dce35753c2ed902064e4d9190366
wuppsn <wuppsn@gmail.com>
2022-04-20 19:32:16 +0700

    [ADD] UAT login regis

--- tests/UAT/BasicFlowUATTest.php
+++ tests/UAT/BasicFlowUATTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\UAT;
 
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
+use App\Models\User;
 
 class BasicFlowUAT extends TestCase {
 
@@ -18,14 +20,64 @@ class BasicFlowUAT extends TestCase {
     public function test_user_have_zero_test() {
         $this->assertTrue(true);
     }
+
     // add task success
+    public function test_add_task_success() {
+
+    }
+
     // edited task
+    public function test_edited_task() {
+
+    }
+
     // deleted task
+    public function test_deleted_task() {
+
+    }
+
     // regis
+    public function test_regis() {
+        $response = $this->post('/register', [
+            'name' => 'dome',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
     // login
+    public function test_login() {
+        $user = User::factory()->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
     // change name
+    public function test_change_name() {
+
+    }
+
     // change password
+    public function test_change_password() {
+
+    }
+
     // change email
+    public function test_change_email() {
+
+    }
+
     // logout
+    public function test_logout() {
+
+    }
+
 
 }
