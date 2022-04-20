e316b453dc50440c46201be98e19a00af56ae78f
siny2000 <sundress198@gmail.com>
2022-04-20 20:10:13 +0700

    [EDIT] EditTaskBAT

--- tests/BAT/EditTaskBATTest.php
+++ tests/BAT/EditTaskBATTest.php
@@ -1,6 +1,24 @@
 <?php
     
-class EditTaskBAT {
+namespace Tests\BAT;
 
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class EditTaskBAT extends TestCase 
+{
+    public function test_edit_task() {
+        $task = new Task([
+            'id' => 1,
+            'description' => 'Add new Task',
+            'goal_date' => '2021-11-11',
+        ]);
+        $task-> description = 'Edit Task';
+        echo($task);
+        $this -> assertStringConTainsString('Edit Task', $task -> description);
+    }
 
 }
